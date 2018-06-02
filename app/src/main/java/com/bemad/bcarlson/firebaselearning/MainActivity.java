65657399484d019b08f26d2bbdfd06b9bd72e7a2
bcarlson01 <bcarlson@berkeley.edu>
2018-06-01 20:57:34 -0700

    Completed Episode 1

---
+++ app/src/main/java/com/bemad/bcarlson/firebaselearning/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bemad.bcarlson.firebaselearning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * Learning how to use Google Firebase: https://firebase.google.com
+     * Following SimCoder YouTube Tutorial:
+     *      Channel: https://www.youtube.com/channel/UCQ5xY26cw5Noh6poIE-VBog
+     *      Episode 1: https://www.youtube.com/watch?v=9rNpfu187wg&list=PLxabZQCAe5fgLqi5im08UYz7R5nfBQYBs
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
